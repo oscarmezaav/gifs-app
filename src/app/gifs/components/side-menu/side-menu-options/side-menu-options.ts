4a1dbc297fb4d33c23f128af01bc832e3d8c3466
Oscar Meza <omeza@globalsoft.email>
2026-06-09 13:02:26 -0600

    RouterLinks desde componentes hijos

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  icon: string
+  label: string,
+  route: string,
+  subLabel: string,
+}
 
 @Component({
   selector: 'side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
-export class GifsSideMenuOptions {}
+export class GifsSideMenuOptions {
+  menuOptions: MenuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    },
+  ]
+}
--- src/app/gifs/components/side-menu/side-menu.ts
+++ src/app/gifs/components/side-menu/side-menu.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { GifsSideMenuHeader } from "./side-menu-header/side-menu-header";
 import { GifsSideMenuOptions } from "./side-menu-options/side-menu-options";
 
+
 @Component({
   selector: 'gifs-side-menu',
   imports: [GifsSideMenuHeader, GifsSideMenuOptions],
